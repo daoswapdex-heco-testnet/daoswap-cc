88ead54b64bf5d936d78075c19364f9b7060fe1b
daoswap002 <daoswap@daoswap.cc>
2021-08-23 10:04:09 +0800

    alert

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,6 @@
 export const CrowdsaleForAngelContractAddress =
   "0x13ccaEFEe7D035cBeD83ffFAdCE0ba1F7a72b50D";
 
-// export const CrowdsaleForRetailContractAddress =
-//   "0x31202f1e8CE970D19DEDA3a12351c8F56074d89e";
 export const CrowdsaleForRetailContractAddress =
   "0x6D28D5c3601Af59A4d4B1A8c4a3963b18B63602d";
 
@@ -16,7 +14,7 @@ export const AirdropToRelationshipContractAddress =
   "0xF154318b83CE7EB7B95039ba51451e1E6fD53B83";
 
 export const AirdropForStakeContractAddress =
-  "0x335cd079ca888533f21aAb5E506832e2aCCdC460";
+  "0x2425F7014d3434a3f0f96639c59D03Abef1a27F0";
 
 export const DATAddress = "0x36653A4089DEB09A4782bf9EaEE5C8f4381ad010";
 
